6d69d35cd63b825984574d5d2bcaa398cf134612
Thomas D <alexander.darracq@gmail.com>
2022-10-20 14:00:57 +0200

    First commit

---
+++ src/main/java/com/project/debiterProject/DebiterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.debiterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebiterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebiterProjectApplication.class, args);
+	}
+
+}
